a3edcf4e1f9f9390bf7e261f368f3b5481afc061
Haodong Liu <haodongliu@Haodongs-MacBook-Pro.local>
2014-09-11 05:09:30 -0400

    Added Calculator Functionality

--- homework/2/webapps/calculator/views.py
+++ homework/2/webapps/calculator/views.py
@@ -16,7 +16,82 @@ def hello(request):
 
 def calculator(request):
 	context = {};
-	context['button_clicked'] = '';
+	context['button_clicked'] = ''
+	context['result'] = ''
+	context['num_displayed']=''
+	context['hidden_value']=''
+
 	if 'button_clicked' in request.POST:
+
 		context['button_clicked'] = request.POST['button_clicked']
+		context['num_displayed'] = request.POST['num_displayed']
+		context['hidden_value'] = request.POST['hidden_value']
+		context['hidden_operator'] = request.POST['hidden_operator']
+		context['hidden_lastbutton'] = request.POST['hidden_lastbutton']
+
+		operand1 = context['hidden_value']
+		operand2 = context['num_displayed']
+		operator = context['hidden_operator']
+
+		#A number is clicked
+		if not isOperatorButton(context['button_clicked']):
+			if isOperatorButton(context['hidden_lastbutton']):
+				context['result'] = context['button_clicked']
+				context['hidden_lastbutton'] = context['button_clicked']
+			else:
+				context['result'] = context['num_displayed'] + context['button_clicked']
+				context['hidden_lastbutton'] = context['button_clicked']
+		#An operator is clicked
+		else:
+			if context['button_clicked'] == 'c':
+				context['result'] = ''
+				context['hidden_value']=''
+				context['hidden_lastbutton']=''
+				context['hidden_operator'] =''
+			else:
+				context['result'] = calculate(operand1,operand2,operator)
+				context['hidden_value'] = context['result']
+				context['hidden_operator'] = context['button_clicked']
+				context['hidden_lastbutton'] = context['button_clicked']
+		
+
+	else:
+		operand1 = 0
+		operand2 = 0
+		operator = ''
+	
 	return render(request,'generic-calculator.html',context)
+
+
+def isOperatorButton(post_parameters):
+	button_clicked = ''
+	operator_set = set()
+	operator_set.add('+')
+	operator_set.add('-')
+	operator_set.add('*')
+	operator_set.add('/')
+	operator_set.add('c')
+	operator_set.add('=')
+
+	if post_parameters in operator_set:
+			return True
+
+	return False
+
+def calculate(operand1, operand2, operator):
+	if operator == '':
+		return operand2
+	elif operator == '+':
+		return int(operand1)+int(operand2)
+	elif operator == '-':
+		return int(operand1)-int(operand2)
+	elif operator == '*':
+		return int(operand1)*int(operand2)
+	elif operator == '/':
+		return int(operand1)/int(operand2)
+	elif operator == 'c':
+		return ''
+	elif operator == '=':
+		return operand2
+	else: 
+		return ''
