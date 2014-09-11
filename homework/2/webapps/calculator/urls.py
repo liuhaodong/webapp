f8b60836cb0c02f45a5bf5b0b81bc47ba0533789
Haodong Liu <haodongliu@Haodongs-MacBook-Pro.local>
2014-09-11 01:38:48 -0400

    Added basic framework files for this homework.

---
+++ homework/2/webapps/calculator/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^hello-world$', 'calculator.views.hello_world'),
+    url(r'^hello.html$', 'calculator.views.hello'),
+)
+
