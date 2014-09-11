ddb89a5e1cb4d262ed6769d20592e4cd186f9849
Haodong Liu <haodongliu@Haodongs-MacBook-Pro.local>
2014-09-11 02:32:14 -0400

    Added basic layout of the calculator.

--- homework/2/webapps/calculator/urls.py
+++ homework/2/webapps/calculator/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^hello-world$', 'calculator.views.hello_world'),
     url(r'^hello.html$', 'calculator.views.hello'),
+    url(r'^$', 'calculator.views.calculator'),
 )
 
--- homework/2/webapps/calculator/views.py
+++ homework/2/webapps/calculator/views.py
@@ -12,3 +12,11 @@ def hello(request):
 		context['person_name'] = request.GET['username']
 
 	return render(request,'greet.html',context)
+
+
+def calculator(request):
+	context = {};
+	context['button_clicked'] = '';
+	if 'button_clicked' in request.POST:
+		context['button_clicked'] = request.POST['button_clicked']
+	return render(request,'generic-calculator.html',context)
