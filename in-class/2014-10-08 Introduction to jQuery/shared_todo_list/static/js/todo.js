d24924de722622cac06faf9951f0043e948c672a
Haodong Liu <haodongliu@Haodongs-MBP.wv.cc.cmu.edu>
2014-10-08 16:44:59 -0400

    Implement excercise

--- in-class/2014-10-08 Introduction to jQuery/shared_todo_list/static/js/todo.js
+++ in-class/2014-10-08 Introduction to jQuery/shared_todo_list/static/js/todo.js
@@ -1,4 +1,10 @@
 // Insert code here to run when the DOM is ready
 $(document).ready( function() {
-
+	$("button").click(function() {
+		var button_id = $(this).attr('data-item-id');
+		var delete_uri = "delete-item/";
+		delete_uri = delete_uri + (button_id);
+		$.get(delete_uri);
+		$(this).parent().remove();
+	});
 });
